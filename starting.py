c2df266871a2c570112fcdda1e69f118b0919789
alfarozavalae <Newgirl1>
2018-11-16 17:59:42 -0500

    pushing main

---
+++ starting.py
@@ -0,0 +1,46 @@
+######################################################################
+# Author: Ela Jamali & Emely Alfaro Zavala
+# Username: Jamalie & Alfarozavalae
+#
+# Assignment: Final Project
+#
+# Purpose: Our program will receive a ciphered message with different characters and will extract the the 12-digit
+# codes found and check for a barcode, afterwards, using the urllib our program will access the web showing the item
+# online.
+####################################################################################
+# Acknowledgements:
+
+####################################################################################
+import turtle
+import urllib
+
+Class Barcode:
+
+
+
+Class CeasarCipher:
+    """
+    The CaesarCipher class represents and manipulates key, input file and the crypt type
+    This class will import a file that needs to be encrypted or decrypted, with an integer representing the key
+    it will decrypt or decrypt the given file
+    """
+    def __init__(self, input_file, key, crypt_type, cypher, import_file):
+
+    def import_file():
+    """
+    """
+    def export_file():
+        """
+
+        :return:
+        """
+    def decrypt():
+        """
+
+        :return:
+        """
+    def online_search():
+        """
+
+        :return:
+        """
