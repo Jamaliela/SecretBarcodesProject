73142dd9c78bb0be1c6350d7285a44b5f49a46a3
alfarozavalae <Newgirl1>
2018-12-01 16:12:15 -0500

    ela is annoying. kill me now hahhahh

--- final_project.py
+++ final_project.py
@@ -15,18 +15,25 @@
 
 import Class_Caesar_cipher as cipher
 import Class_barcode as barcode
-#
+import turtle
 
 def main():
 
-    wn = self.tess.Turtle
+    wn = turtle.Screen()
+
     text1 = cipher.CeasarCipher("ciphertext.txt", 7, "decrypt")        # assigning text1 the class and opening the message to decrypt
     exporting = text1.decrypt()                                 # decrypting the file
     text1.export_file(exporting, "exporting_text")              # naming the new file that has been decrypted
     print(text1.search_numbers())
     upc_barcode = barcode.Barcode()
     upc_barcode.is_valid_input()
-    # upc_barcode.is_valid_modulo()
-    # upc_barcode.translate()
+    upc_barcode.is_valid_modulo()
+    upc_barcode.translate()
+    upc_barcode.drawing_blackline()
+    upc_barcode.drawing_blackline_long()
+    upc_barcode.drawing_white_line()
+    upc_barcode.drawing_blackline()
+    wn.exitonclick()
+
 
 main()
